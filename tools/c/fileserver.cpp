116b7cd9f208fa0a09e96b86c980f033d7da491f
leeczx <leeczxc@outlook.com>
2023-07-13 18:46:46 +0800

    tcp 2023-07-13

--- tools/c/fileserver.cpp
+++ tools/c/fileserver.cpp
@@ -5,6 +5,25 @@
 
 #include "_public.h"
 
+// 程序运行的参数结构体
+struct st_arg
+{
+    int clienttype;       // 客户端类型， 1-上传文件，2-下载文件
+    char ip[31];          // 服务端ip地址
+    int port;             // 服务端的接口
+    int ptype;            // 文件上传成功后文件的处理方式: 1-删除文件 2-移动到备份目录
+    char clientpath[301]; // 客户端文件存放的根目录
+    bool andchild;        // 是否上传clientpath目录下各级子目录的文件， true 是 false 否
+    char matchname[301];  //  待上传文件民改得匹配方式， 如 “*.txt, *.xml.”大小写有效
+    char srvpath[301];    // 服务端文件存放的根目录
+    char srvpathbak[301]; // 服务端文件存放的根目录
+    int timetvl;          // 扫描本地目录文件的时间间隔，单位，秒
+    int timeout;          // 进程心跳的超时时间
+    char pname[51];       // 进程名，建议用"tcpgetifiles_后缀"的方式
+} starg;
+
+bool _xmltoarg(char *strxmlbuffer);
+
 CLogFile logfile;     // 服务程序的运行日志
 CTcpServer TcpServer; // 船舰服务端对象
 
@@ -14,24 +33,31 @@ void ChldEXIT(int sig); // 子进程退出函数
 char strrecvbuffer[1024]; // 发送报文的buffer
 char strsendbuffer[1024]; // 接受保文的buffer
 // 处理业务的主函数
-bool _main(const char *strrecvbuffer, char *strsendbuffer);
+// bool _main(const char *strrecvbuffer, char *strsendbuffer);
 
 // 心跳
-bool srv000(const char *strrecvbuffer, char *strsendbuffer);
+// bool srv000(const char *strrecvbuffer, char *strsendbuffer);
 
 // 登录业务处理函数
-bool srv001(const char *strrecvbuffer, char *strsendbuffer);
+bool ClientLogin();
+
+// 调用文件上传的主函数
+void RecvFilesMain();
 
 int main(int argc, char *argv[])
 {
     if (argc != 4)
     {
-        printf("Using:./fileserver port logfile timeout.\n Example:./fileserver 5005 ../log/test 35\n\n");
+        printf("Using:./fileserver port logfile\n Example:./fileserver 5005 ../log/tcp\n\n");
         return -1;
     }
     // 关闭全部的信号和输入输出。
     // 设置信号，在shell状态下可用"kill + 进程号"正常终止进程
     // 不要用“kill -9 + 进程号”强行终止
+    CloseIOAndSignal();
+    signal(SIGINT, FathEXIT);
+    signal(SIGTERM, FathEXIT);
+
     if (logfile.Open(argv[2], "a+") == false)
     {
         printf("logfile.Open(%s) failed.\n", argv[2]);
@@ -39,7 +65,7 @@ int main(int argc, char *argv[])
     }
 
     // 服务端初始化
-    if (TcpServer.InitServer(atoi(argv[1]) == false))
+    if (TcpServer.InitServer(atoi(argv[1])) == false)
     {
         logfile.Write("TcpServer.InitServer(%s) failed.\n", argv[1]);
         return -1;
@@ -67,32 +93,45 @@ int main(int argc, char *argv[])
 
         // TcpServer.CloseListen();
 
+        // 处理登录客户端的登录报文
+
         // 子进程与客户端进行通讯，处理业务
+        if (ClientLogin() == false)
+        {
+            ChldEXIT(-1);
+        }
+        // 如果clienttype == 1， 调用上传文件的主函数
+        if (starg.clienttype == 1)
+        {
+            RecvFilesMain();
+        }
+        // 如果clienttype == 2， 调用下载文件的主函数
 
         // 与客户端通讯，接受客户端发过来的保文，回复ok
-        while (1)
-        {
-            memset(strrecvbuffer, 0, sizeof(strrecvbuffer));
-            memset(strsendbuffer, 0, sizeof(strsendbuffer));
+        // while (1)
+        // {
+        //     memset(strrecvbuffer, 0, sizeof(strrecvbuffer));
+        //     memset(strsendbuffer, 0, sizeof(strsendbuffer));
 
-            if (TcpServer.Read(strrecvbuffer, atoi(argv[3])) == false)
-            {
-                break;
-            }
-            logfile.Write("接受:%s\n", strrecvbuffer);
+        //     if (TcpServer.Read(strrecvbuffer, atoi(argv[3])) == false)
+        //     {
+        //         break;
+        //     }
+        //     logfile.Write("接受:%s\n", strrecvbuffer);
 
-            // 处理业务的主函数
-            if (_main(strrecvbuffer, strsendbuffer) == false)
-            {
-                break;
-            }
+        //     // 处理业务的主函数
+        //     if (_main(strrecvbuffer, strsendbuffer) == false)
+        //     {
+        //         break;
+        //     }
 
-            if (TcpServer.Write(strsendbuffer) == false)
-            {
-                break;
-            }
-            logfile.Write("发送:%s\n", strsendbuffer);
-        }
+        //     if (TcpServer.Write(strsendbuffer) == false)
+        //     {
+        //         break;
+        //     }
+        //     logfile.Write("发送:%s\n", strsendbuffer);
+        // }
+
         ChldEXIT(-1);
     }
 }
@@ -106,7 +145,7 @@ void FathEXIT(int sig)
 
     logfile.Write("父进程退出， sig=%d.\n", sig);
 
-    TcpServer.CloseListen(); // 关闭坚挺的socker.
+    TcpServer.CloseListen(); // 关闭监听的socker.
 
     kill(0, 15); // 通知全部的子进程全部退出
     exit(0);
@@ -125,44 +164,162 @@ void ChldEXIT(int sig)
 }
 
 // 处理业务的主函数
-bool _main(const char *strrecvbuffer, char *strsendbuffer)
-{
-    // 解析strrecvbuffer，获取服务代码（业务代码)
-    int isrvcode = -1;
-    GetXMLBuffer(strrecvbuffer, "srvcode", &isrvcode);
+// bool _main(const char *strrecvbuffer, char *strsendbuffer)
+// {
+//     // 解析strrecvbuffer，获取服务代码（业务代码)
+//     int isrvcode = -1;
+//     GetXMLBuffer(strrecvbuffer, "srvcode", &isrvcode);
 
-    if (isrvcode != 1)
-    {
-        strcpy(strsendbuffer, "<retcode>-1</retcode><message>用户未登录。</message>");
-        return true;
-    }
-    switch (isrvcode)
-    {
-    case 1: // 登录
-        srv001(strrecvbuffer, strsendbuffer);
-        break;
-    default:
-        logfile.Write("业务代码不合适:%s\n", strrecvbuffer);
-        return false;
-    }
-}
+//     if (isrvcode != 1)
+//     {
+//         strcpy(strsendbuffer, "<retcode>-1</retcode><message>用户未登录。</message>");
+//         return true;
+//     }
+//     switch (isrvcode)
+//     {
+//     case 1: // 登录
+//         srv001(strrecvbuffer, strsendbuffer);
+//         break;
+//     default:
+//         logfile.Write("业务代码不合适:%s\n", strrecvbuffer);
+//         return false;
+//     }
+//     return true;
+// }
+
+// // 心跳
+// bool srv000(const char *strrecvbuffer, char *strsendbuffer)
+// {
+//     strcpy(strsendbuffer, "<retcode>0</retcode><message>成功。</message>");
+//     return true;
+// }
 
 // 登录
-bool srv001(const char *strrecvbuffer, char *strsendbuffer)
+bool ClientLogin()
 {
     // <srvcode>1</srvcode><tel>1392220000</tel><password>123456</password>
 
     // 解析strrecvbuffer,获取业务参数
-    char tel[21], password[21];
-    GetXMLBuffer(strrecvbuffer, "tel", tel, 20);
-    GetXMLBuffer(strrecvbuffer, "password", password, 20);
-    // 处理业务。 35   // 把处理结果生成strsendbuffer。
-    if ((strcmp(tel, "1392220000") == 0) && (strcmp(password, "123456") == 0))
+    memset(strsendbuffer, 0, sizeof(strsendbuffer));
+    memset(strrecvbuffer, 0, sizeof(strrecvbuffer));
+
+    if (TcpServer.Read(strrecvbuffer, 20) == false)
     {
-        strcpy(strsendbuffer, "<retcode>0</retcode><message>成功。</message>");
+        logfile.Write("TcpServer Read() failed.\n");
+        return false;
+    }
+
+    logfile.Write("strrecvbuffer = %s\n", strrecvbuffer);
+
+    // 解析客户端登录报文
+    _xmltoarg(strrecvbuffer);
+    if ((starg.clienttype == 1) || (starg.clienttype == 2))
+    {
+        strcpy(strsendbuffer, "ok");
     }
     else
-        strcpy(strsendbuffer, "<retcode>-1</retcode><message>失败。</message>");
+    {
+        strcpy(strsendbuffer, "failed");
+    }
+
+    if (TcpServer.Write(strsendbuffer) == false)
+    {
+        logfile.Write("TcpServer.Write(strsendbuffer) failed\n");
+        return false;
+    }
+
+    logfile.Write("%s login %s.\n", TcpServer.GetIP(), strrecvbuffer);
+    // char tel[21], password[21];
+    // GetXMLBuffer(strrecvbuffer, "tel", tel, 20);
+    // GetXMLBuffer(strrecvbuffer, "password", password, 20);
+    // // 处理业务。 35   // 把处理结果生成strsendbuffer。
+    // if ((strcmp(tel, "1392220000") == 0) && (strcmp(password, "123456") == 0))
+    // {
+    //     strcpy(strsendbuffer, "<retcode>0</retcode><message>成功。</message>");
+    // }
+    // else
+    //     strcpy(strsendbuffer, "<retcode>-1</retcode><message>失败。</message>");
 
     return true;
+}
+
+// 把xml解析到参数starg结构中 47 bool _xmltoarg(char *strxmlbuffer)
+bool _xmltoarg(char *strxmlbuffer)
+{
+    memset(&starg, 0, sizeof(struct st_arg));
+
+    // 不需要对参数做合法性判断，客户端已经判断过了。
+    GetXMLBuffer(strxmlbuffer, "clienttype", &starg.clienttype);
+    GetXMLBuffer(strxmlbuffer, "ptype", &starg.ptype);
+    GetXMLBuffer(strxmlbuffer, "clientpath", starg.clientpath);
+    GetXMLBuffer(strxmlbuffer, "andchild", &starg.andchild);
+    GetXMLBuffer(strxmlbuffer, "matchname", starg.matchname);
+    GetXMLBuffer(strxmlbuffer, "srvpath", starg.srvpath);
+    GetXMLBuffer(strxmlbuffer, "srvpathbak", starg.srvpathbak);
+
+    GetXMLBuffer(strxmlbuffer, "timetvl", &starg.timetvl);
+    if (starg.timetvl > 30)
+        starg.timetvl = 30;
+
+    GetXMLBuffer(strxmlbuffer, "timeout", &starg.timeout);
+    if (starg.timeout < 50)
+        starg.timeout = 50;
+
+    GetXMLBuffer(strxmlbuffer, "pname", starg.pname, 50);
+    strcat(starg.pname, "_srv");
+
+    return true;
+}
+
+// 调用文件上传的主函数
+void RecvFilesMain()
+{
+    while (true)
+    {
+        // 接受客户端的报文
+        memset(strsendbuffer, 0, sizeof(strsendbuffer));
+        memset(strrecvbuffer, 0, sizeof(strrecvbuffer));
+
+        if (TcpServer.Read(strrecvbuffer, starg.timetvl + 10) == false)
+        {
+            logfile.Write("TcpServer.Read() failed.\n");
+            return;
+        }
+        logfile.Write("strrecvbuffer=%s\n", strrecvbuffer);
+
+        // 处理心跳报文
+        if (strcmp(strrecvbuffer, "<activetest>ok</activetest>") == 0)
+        {
+            strcpy(strsendbuffer, "ok");
+            logfile.Write("strsendbuffer=%s\n", strsendbuffer);
+            if (TcpServer.Write(strsendbuffer) == false)
+            {
+                logfile.Write("TcpServer.Write() failed.\n");
+                return;
+            }
+        }
+        // 处理上传文件的请求报文
+        if(strncmp(strrecvbuffer, "<filename>", 10) == 0){
+            // 解析上传文件请求报文的xml
+            char clientfilename[301];
+            memset(clientfilename, 0, sizeof(clientfilename));
+            char mtime[21];
+            memset(mtime, 0, sizeof(mtime));
+            int filesize = 0;
+            GetXMLBuffer(strrecvbuffer, "filename", clientfilename, 300);
+            GetXMLBuffer(strrecvbuffer, "mtime", mtime, 20);
+            GetXMLBuffer(strrecvbuffer, "size", &filesize);
+
+            // 接受上传文件的内容
+            SNPRINTF(strsendbuffer, sizeof(strsendbuffer), 1000, "<filename>%s</filename><result>ok</result>", strsendbuffer);
+
+            // 把接受结果返回给对端
+            logfile.Write("strsendbuffer=%s\n", strsendbuffer);
+            if (TcpServer.Write(strsendbuffer) == false)
+            {
+                logfile.Write("TcpServer.Write() failed.\n");
+                return;
+            }
+        }
+    }
 }
--- tools/c/tcpputfiles.cpp
+++ tools/c/tcpputfiles.cpp
@@ -1,76 +1,310 @@
 /*
  * 程序名: 采用tcp协议，实现文件上传的客户端
-  * author: Leeczxc
-*/
+ * author: Leeczxc
+ */
 
 #include "_public.h"
 
+// 程序运行的参数结构体
+struct st_arg
+{
+    int clienttype;          // 客户端类型， 1-上传文件，2-下载文件
+    char ip[31];             // 服务端ip地址
+    int port;                // 服务端的接口
+    int ptype;               // 文件上传成功后文件的处理方式: 1-删除文件 2-移动到备份目录
+    char clientpath[301];    // 本地文件存放的根目录
+    char clientpathbak[301]; // 文件成功上传后，本地文件备份的根目录，当ptype == 2时有效
+    bool andchild;           // 是否上传clientpath目录下各级子目录的文件， true 是 false 否
+    char matchname[301];     //  待上传文件民改得匹配方式， 如 “*.txt, *.xml.”大小写有效
+    char srvpath[301];       // 服务端文件存放的根目录
+    int timetvl;             // 扫描本地目录文件的时间间隔，单位，秒
+    int timeout;             // 进程心跳的超时时间
+    char pname[51];          // 进程名，建议用"tcpgetifiles_后缀"的方式
+} starg;
+
+// 程序退出和信号2、15的处理函数
+void EXIT(int sig);
+
+void _help();
+
+// 登录业务
+bool Login(const char *argv);
+// 把xm解析到参数starg结构中
+bool _xmltoarg(char *strxmlbuffer);
+// 日志文件
+CLogFile logfile;
+
+CPActive PActive; // 进程心跳
+
 CTcpClient TcpClient;
 
-bool srv000(); // 心跳
-bool srv001(); // 登录
+char strrecvbuffer[1024]; // 发送报文的buffer
+char strsendbuffer[1024]; // 接受保文的buffer
 
-int main(int argc, char* argv[]){
-    if(argc != 3){
-        printf("Using:./tcpputfiles ip port \n Examplae:./tcpputfiles localhost 5006\n\n");
+bool ActiveTest(); // 心跳
+
+ // 调用文件上传的主函数，执行一次文件上传的任务
+bool _tcpputfiles();
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        _help();
+        return -1;
+    }
+
+    // 关闭全部的信号和输入输出。
+    // 设置信号，在shell状态下可用"kill + 进程号"正常终止进程
+    // 不要用“kill -9 + 进程号”强行终止
+    // CloseIOAndSignal();
+    // signal(SIGINT, EXIT);
+    // signal(SIGTERM, EXIT);
+
+    if (logfile.Open(argv[1], "a+") == false)
+    {
+        printf("open logfile error!\n");
+        return -1;
+    }
+
+    // 解析xml，得到程序运行的参数
+    if (_xmltoarg(argv[2]) == false)
+    {
         return -1;
     }
     // 向服务端发送连接请求
-    if(TcpClient.ConnectToServer(argv[1], atoi(argv[2])) == false){
-        printf("TcpClient.ConnectToServer(%s, %s) failed.\n", argv[1], argv[2]);
-        return -1;
+    if (TcpClient.ConnectToServer(starg.ip, starg.port) == false)
+    {
+        logfile.Write("TcpClient.ConnectToServer(%s, %s) failed.\n", starg.ip, starg.port);
+        EXIT(-1);
     }
 
     // 登录业务
-    if(srv001() == false){
-        printf("srv001() failed.\n");
-        return -1;
+    if (Login(argv[2]) == false)
+    {
+        logfile.Write("Login() failed.\n");
+        EXIT(-1);
     }
 
-    sleep(3);
+    while(true){
+        // 调用文件上传的主函数，执行一次文件上传的任务
+        if(_tcpputfiles() ==false){
+            logfile.Write("_tcpputfiles() failed.\n");
+            EXIT(-1);
+        }
+        sleep(starg.timetvl);
+        if(ActiveTest() == false){
+            break;
+        }
+    }
 
-    // 我的账户（查询余额）
+    // for (int ii = 3; ii < 5; ii++)
+    // {
+    //     if (ActiveTest() == false)
+    //         break;
+    //     sleep(ii);
+    // }
+    // while(true){
+    //     PActive.UptATime();
+    // }
+    // EXIT(0);
+    EXIT(0);
 }
 
 // 心跳
-bool srv000(){
-    char buffer[1024];
-    SPRINTF(buffer, sizeof(buffer), "<srvcode>0</srvcode>");
-    printf("发送: %s\n", buffer);
-    if(TcpClient.Write(buffer) == false){ // 向服务端发送请求报文
+bool ActiveTest()
+{
+    memset(strsendbuffer, 0, sizeof(strsendbuffer));
+    memset(strrecvbuffer, 0, sizeof(strrecvbuffer));
+    SPRINTF(strsendbuffer, sizeof(strsendbuffer), "<activetest>ok</activetest>");
+    logfile.Write("发送: %s\n", strsendbuffer);
+    if (TcpClient.Write(strsendbuffer) == false)
+    { // 向服务端发送请求报文
+        logfile.Write("心跳 tcpClient.Write(buffer) failed\n");
         return false;
     }
 
-    memset(buffer, 0, sizeof(buffer));
-    if(TcpClient.Read(buffer) == false){ // 接受服务端的回应报文
+    if (TcpClient.Read(strrecvbuffer, 20) == false)
+    { // 接受服务端的回应报文
+        logfile.Write("心跳 tcpClient.Read(strrecvbuffer, 20) failed\n");
+        return false;
+    }
+    logfile.Write("接受: %s\n", strrecvbuffer);
+    return true;
+}
+
+bool Login(const char *argv)
+{
+
+    memset(strsendbuffer, 0, sizeof(strsendbuffer));
+    memset(strrecvbuffer, 0, sizeof(strrecvbuffer));
+
+    SPRINTF(strsendbuffer, sizeof(strsendbuffer), "%s<clienttype>1</clienttype>", argv);
+
+    logfile.Write("发送: %s\n", strsendbuffer);
+    if (TcpClient.Write(strsendbuffer) == false)
+    {
+        logfile.Write("TcpClient.Write(strsendbuffer) failed\n");
+        return false;
+    }
+
+    if (TcpClient.Read(strrecvbuffer, 20) == false)
+    { // 接受服务端的回应报文
+        logfile.Write("TcpClient.Read(strrecvbuffer, 20) failed\n");
+        return false;
+    }
+    logfile.Write("接受: %s\n", strrecvbuffer);
+
+    logfile.Write("登录(%s:%d)成功.\n", starg.ip, starg.port);
+    return true;
+}
+
+void EXIT(int sig)
+{
+    logfile.Write("程序退出, sig = %d\n\n", sig);
+    exit(0);
+}
+
+void _help()
+{
+    printf("\n");
+    printf("Using:/project/tools1/bin/tcpputfiles logfilename xmlbuffer\n\n");
+
+    printf("Sample:/project/tools1/bin/procctl 20 /project/tools1/bin/tcpputfiles /log/idc/tcpputfiles_surfdata.log \"<ip>192.168.174.133</ip><port>5005</    port><ptype>1</ptype><clientpath>/tmp/tcp/surfdata1</clientpath><andchild>true</andchild><matchname>*.XML,*.CSV,*.JSON</matchname><srvpath>/tmp/tcp/surf    data2</srvpath><timetvl>10</timetvl><timeout>50</timeout><pname>tcpputfiles_surfdata</pname>\"\n");
+    printf("       /project/tools1/bin/procctl 20 /project/tools1/bin/tcpputfiles /log/idc/tcpputfiles_surfdata.log \"<ip>192.168.174.132</ip><port>5005</    port><ptype>2</ptype><clientpath>/tmp/tcp/surfdata1</clientpath><clientpathbak>/tmp/tcp/surfdata1bak</clientpathbak><andchild>true</andchild><matchname>    *.XML,*.CSV,*.JSON</matchname><srvpath>/tmp/tcp/surfdata2</srvpath><timetvl>10</timetvl><timeout>50</timeout><pname>tcpputfiles_surfdata</pname>\"\n\n\n    ");
+
+    printf("本程序是数据中心的公共功能模块，采用tcp协议把文件上传给服务端。\n");
+    printf("logfilename   本程序运行的日志文件。\n");
+    printf("xmlbuffer     本程序运行的参数，如下：\n");
+    printf("ip            服务端的IP地址。\n");
+    printf("port          服务端的端口。\n");
+    printf("ptype         文件上传成功后的处理方式：1-删除文件；2-移动到备份目录。\n");
+    printf("clientpath    本地文件存放的根目录。\n");
+    printf("clientpathbak 文件成功上传后，本地文件备份的根目录，当ptype==2时有效。\n");
+    printf("andchild      是否上传clientpath目录下各级子目录的文件，true-是；false-否，缺省为false。\n");
+    printf("matchname     待上传文件名的匹配规则，如\"*.TXT,*.XML\"\n");
+    printf("srvpath       服务端文件存放的根目录。\n");
+    printf("timetvl       扫描本地目录文件的时间间隔，单位：秒，取值在1-30之间。\n");
+    printf("timeout       本程序的超时时间，单位：秒，视文件大小和网络带宽而定，建议设置50以上。\n");
+    printf("pname         进程名，尽可能采用易懂的、与其它进程不同的名称，方便故障排查。\n\n");
+}
+
+// 把xml解析到参数starg结构
+bool _xmltoarg(char *strxmlbuffer)
+{
+    memset(&starg, 0, sizeof(struct st_arg));
+
+    GetXMLBuffer(strxmlbuffer, "ip", starg.ip);
+    if (strlen(starg.ip) == 0)
+    {
+        logfile.Write("ip is null.\n");
+        return false;
+    }
+
+    GetXMLBuffer(strxmlbuffer, "port", &starg.port);
+    if (starg.port == 0)
+    {
+        logfile.Write("port is null.\n");
+        return false;
+    }
+
+    GetXMLBuffer(strxmlbuffer, "ptype", &starg.ptype);
+    if ((starg.ptype != 1) && (starg.ptype != 2))
+    {
+        logfile.Write("ptype not in (1,2).\n");
+        return false;
+    }
+
+    GetXMLBuffer(strxmlbuffer, "clientpath", starg.clientpath);
+    if (strlen(starg.clientpath) == 0)
+    {
+        logfile.Write("clientpath is null.\n");
+        return false;
+    }
+
+    GetXMLBuffer(strxmlbuffer, "clientpathbak", starg.clientpathbak);
+    if ((starg.ptype == 2) && (strlen(starg.clientpathbak) == 0))
+    {
+        logfile.Write("clientpathbak is null.\n");
+        return false;
+    }
+
+    GetXMLBuffer(strxmlbuffer, "andchild", &starg.andchild);
+
+    GetXMLBuffer(strxmlbuffer, "matchname", starg.matchname);
+    if (strlen(starg.matchname) == 0)
+    {
+        logfile.Write("matchname is null.\n");
+        return false;
+    }
+
+    GetXMLBuffer(strxmlbuffer, "srvpath", starg.srvpath);
+    if (strlen(starg.srvpath) == 0)
+    {
+        logfile.Write("srvpath is null.\n");
+        return false;
+    }
+
+    GetXMLBuffer(strxmlbuffer, "timetvl", &starg.timetvl);
+    if (starg.timetvl == 0)
+    {
+        logfile.Write("timetvl is null.\n");
+        return false;
+    }
+
+    // 扫描本地目录文件的时间间隔，单位：秒。 15   // starg.timetvl没有必要超过30秒。
+    if (starg.timetvl > 30)
+        starg.timetvl = 30;
+
+    // 进程心跳的超时时间，一定要大于starg.timetvl，没有必要小于50秒。
+    GetXMLBuffer(strxmlbuffer, "timeout", &starg.timeout);
+    if (starg.timeout == 0)
+    {
+        logfile.Write("timeout is null.\n");
+        return false;
+    }
+    if (starg.timeout < 50)
+        starg.timeout = 50;
+
+    GetXMLBuffer(strxmlbuffer, "pname", starg.pname, 50);
+    if (strlen(starg.pname) == 0)
+    {
+        logfile.Write("pname is null.\n");
         return false;
     }
 
     return true;
 }
-
-bool srv001(){
-    char buffer[1024];
-
-    SPRINTF(buffer, sizeof(buffer), "<srvcode>1</srvcode><tel>1392220000</tel><password>123456</password>");
-    printf("发送：%s\n", buffer);
-    if(TcpClient.Write(buffer) == false){
-        return false;
+ 
+// 调用文件上传的主函数，执行一次文件上传的任务
+bool _tcpputfiles(){
+    CDir Dir;
+    // 调用OpenDir()大佬starg.clientpath目录
+    if(Dir.OpenDir(starg.clientpath, starg.matchname, 10000, starg.andchild) == false){
+        logfile.Write("Dir.openDir(%s) 失败\n", starg.clientpath);
     }
+    while(true){
+        // 遍历目录中的每个文件，调用ReadDir()获取一个文件名
+        if(Dir.ReadDir() == false){
+            break;
+        }
 
-    memset(buffer, 0, sizeof(buffer));
-    if(TcpClient.Read(buffer) == false){ // 接受服务端的回应报文
-        return false;
+        // 把文件名、修改时间、文件大小组成报文，发送给对端
+        SNPRINTF(strsendbuffer, sizeof(strsendbuffer), 1000, "<filename>%s</filename><mtime>%s</mtime><size>%d</size>",
+        Dir.m_FullFileName, Dir.m_ModifyTime, Dir.m_FileSize);
+        logfile.Write("strsendbuffer=%s\n", strsendbuffer);
+        if(TcpClient.Write(strsendbuffer) == false){
+            logfile.Write("TcpClient.Write() failed.\n");
+            return false;
+        }
+        // 把文件的内容发送给对端
+        // 接收对端的确认报文
+        if(TcpClient.Read(strrecvbuffer, 20) ==false){
+            logfile.Write("TcpClient.Read() failed.\n");
+            return false;
+        }
+        logfile.Write("strrecvbuffer=%s\n", strrecvbuffer);
+        // 删除或者转存本地的文件
     }
-    printf("接受:%s\n", buffer);
-
-    // 解析服务器端返回的xml
-    int iretcode = -1;
-    GetXMLBuffer(buffer, "remote", &iretcode);
-    if(iretcode != 0){
-        printf("登录失败.\n");
-        return false;
-    }
-    printf("登录成功.\n");
     return true;
-}
+} 
